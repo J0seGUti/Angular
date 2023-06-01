9ff7f98f2fdc7b7c7940f3d10effdaed7894ebbc
J0seGUti <125706410+J0seGUti@users.noreply.github.com>
2023-06-01 18:55:26 -0500

    Add files via upload

---
+++ src/app/inicio/inicio.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import 'bootstrap/dist/js/bootstrap.bundle';
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.component.html',
+  styleUrls: ['./inicio.component.scss']
+})
+export class InicioComponent {
+
+}
